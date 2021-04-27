df94c6756abe3a2caef5650f19e3268107506626
Leon <leonxu0204@gmail.com>
2021-04-26 23:01:36 -0400

    Improved efficiency of the program

--- main.js
+++ main.js
@@ -1,56 +1,47 @@
-var array = [];
-for (var i = 2; i < process.argv.length; i++)
-{ // push the arguments into an array
-    array.push(process.argv[i]);
-}
-var String = convert(array);
-console.log(String);
+console.log(format(process.argv.slice(2)));
 
-function convert(array)
+function format(array)
+{ // split the numbers into individual elements in order to be converted and
+    // then joined together at the end
+    return array.join(',').split('').map(int => convert(int)).join('');
+}
+
+function convert(integer)
 { // convert integers into strings representing the number
-    var String = "";
-    for (var i = 0; i < array.length; i++)
-    {
-        for (var j = 0; j < array[i].toString().length; j++)
-        {
-            switch (array[i].toString().charAt(j))
-            { // check each digit of the string
-                case '0':
-                    String += "Zero";
-                    break;
-                case '1':
-                    String += "One";
-                    break;
-                case '2':
-                    String += "Two";
-                    break;
-                case '3':
-                    String += "Three";
-                    break;
-                case '4':
-                    String += "Four";
-                    break;
-                case '5':
-                    String += "Five";
-                    break;
-                case '6':
-                    String += "Six";
-                    break;
-                case '7':
-                    String += "Seven";
-                    break;
-                case '8':
-                    String += "Eight";
-                    break;
-                case '9':
-                    String += "Nine";
-                    break;
-            }
-        }
-        if (i != array.length - 1)
-        {
-            String += ",";
-        }
+    switch (integer)
+    { // check each digit of the string
+        case '0':
+            return "Zero";
+            break;
+        case '1':
+            return "One";
+            break;
+        case '2':
+            return "Two";
+            break;
+        case '3':
+            return "Three";
+            break;
+        case '4':
+            return "Four";
+            break;
+        case '5':
+            return "Five";
+            break;
+        case '6':
+            return "Six";
+            break;
+        case '7':
+            return "Seven";
+            break;
+        case '8':
+            return "Eight";
+            break;
+        case '9':
+            return "Nine";
+            break;
+        case ',':
+            return ",";
+            break;
     }
-    return String;
 }
